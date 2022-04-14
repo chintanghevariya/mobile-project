37b94774a7ed2cd214f8b8a49acd7ab40f0433a2
AaryTrivedi <aarytrivedi@gmail.com>
2022-04-14 13:27:19 -0400

    Adds custom tag component in search page

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -11,6 +11,8 @@ export class SearchPage implements OnInit {
 
   value: string;
   restaurants: any;
+  currentTagValue: string = "";
+  tags = []
 
   constructor(
     private restaurantService: RestaurantService,
@@ -38,8 +40,24 @@ export class SearchPage implements OnInit {
   navigateToViewRestaurant(id:any){
     this.router.navigateByUrl(`/view-restaurant?id=${Number(id)}`)
   }
+
   navigateToEditRestaurant(id:any){
     this.router.navigateByUrl(`/edit-restuarant?id=${Number(id)}`)
+  }
+
+  addTag() {
+    const tagExists = this.tags.findIndex(tag => {
+      return tag.toLowerCase() === this.currentTagValue.toLowerCase();
+    }) > -1;
+    if (tagExists === false) {
+      this.tags.push(this.currentTagValue);
+      this.currentTagValue = ""
+    }
+  }
+  
+  removeTag(tag:any){
+    const index = this.tags.indexOf(tag)
+    this.tags.splice(index,1)
   }
 
 }
