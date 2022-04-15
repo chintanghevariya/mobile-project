b832e599f196146ffb77273c21db6b0aae8702a8
Rutik Patel <rutikpatel3660@gmail.com>
2022-04-15 16:00:26 -0400

    Adds sending email in email service

--- src/app/services/email/email.service.ts
+++ src/app/services/email/email.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  constructor(
+    private emailComposer: EmailComposer
+  ) { }
+
+  async getClients() {
+    const clients = await this.emailComposer.getClients();
+    return clients;
+  }
+
+  send(app, email) {
+    this.emailComposer.isAvailable().then(app, (available: boolean) => {
+      if (available) {
+        this.emailComposer.open(email);
+      }
+    });
+  }
 }
